82114dd03822f11b1d92666c015f88408f00df6c
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-04-01 19:05:57 +0200

    feat: check if enough arguments, header file

--- fleck.c
+++ fleck.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char VERSION[] = "0.1.0";
+#include "fleck.h"
+#include "util.h"
 
-int main(void) {
-  printf("fleck - %s\n", VERSION);
+int main(int argc, char **argv) {
+  util_print("fleck - " FLECK_VERSION);
+  if (argc < 2) {
+    util_print_err("not enough arguments, exiting");
+  }
+
+  char *inFile = argv[1];
+  if (inFile == NULL) {
+    util_print_err("input file is null");
+  }
   return EXIT_SUCCESS;
 }
---
+++ fleck.h
@@ -0,0 +1,6 @@
+#ifndef FLECK_H
+#define FLECK_H
+
+#define FLECK_VERSION "v0.1.0"
+
+#endif
