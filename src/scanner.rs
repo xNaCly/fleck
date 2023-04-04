4b69262d0ed38195d43368e50ad92a5cb167c45f
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-04-04 08:48:35 +0200

    refactor: removed parsing logic

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,7 +3,7 @@ use std::fs;
 use crate::token::{self, Token};
 
 /// holds variables used to parse the markdown file
-pub struct Parser {
+pub struct Scanner {
     pub current_char: char,
     /// whole file in a string
     pub input: String,
@@ -17,15 +17,15 @@ pub struct Parser {
     pub last_char: char,
 }
 
-impl Parser {
+impl Scanner {
     /// get a new instance of Parser, reads the file with the given file_path and its first character
-    pub fn new(file_path: &str) -> Parser {
+    pub fn new(file_path: &str) -> Scanner {
         let input = fs::read_to_string(file_path).expect("could not read file");
         let current_char = input
             .chars()
             .nth(0)
             .expect("could not get first character of input");
-        Parser {
+        Scanner {
             input,
             current_char,
             last_char: '\0',
@@ -35,7 +35,7 @@ impl Parser {
         }
     }
 
-    /// returns true if the parser is a the end of the file
+    /// returns true if the parser is a the end of the input
     fn at_end(&self) -> bool {
         if self.current_char == '\0' || self.pos >= self.input.len() {
             return true;
@@ -43,7 +43,7 @@ impl Parser {
         return false;
     }
 
-    /// returns the next character without advancing or '\0' if at the end of the file
+    /// returns the next character without advancing or '\0' if at the end of the input
     fn peek(&self) -> char {
         self.input.chars().nth(self.pos + 1).unwrap_or('\0')
     }
@@ -76,265 +76,17 @@ impl Parser {
         }
     }
 
-    /// wrapper for creating a paragraph
-    fn create_paragraph(&mut self, text: &str) -> Token {
-        self.create_token(token::TokenKind::Paragraph, String::from(text))
-    }
-
     /// parses input, returns a vector of tokens
     pub fn parse(&mut self) -> Vec<Token> {
         let mut res: Vec<Token> = vec![];
-        let mut last_paragraph = String::new();
         while !self.at_end() && self.current_char != '\0' {
             let mut token_value = String::new();
-            let mut token_kind = token::TokenKind::Unknown;
+            let mut token_kind = token::TokenKind::Text;
 
             match self.current_char {
-                '\n' => {
-                    if !last_paragraph.is_empty() {
-                        res.push(self.create_paragraph(&last_paragraph));
-                        last_paragraph = String::new();
-                    }
-                    self.line += 1;
-                    self.line_pos = 0;
-                    self.advance();
-                    continue;
-                }
-                '\r' => {
-                    self.advance();
-                    continue;
-                }
-                '#' => {
-                    if self.last_char != '\n' {
-                        last_paragraph.push(self.current_char);
-                        self.advance();
-                        continue;
-                    }
-                    if !(self.line_pos == 0 || self.line_pos == 1) {
-                        last_paragraph.push(self.current_char);
-                        self.advance();
-                        continue;
-                    }
-                    if !last_paragraph.is_empty() {
-                        res.push(self.create_paragraph(&last_paragraph));
-                        last_paragraph = String::new();
-                    }
-
-                    // skip over '#' with a counter:
-                    let mut heading_id = 1;
-                    self.advance();
-                    while self.current_char == '#' {
-                        self.advance();
-                        heading_id += 1;
-                    }
-                    // consume last #
-                    self.advance();
-
-                    while self.current_char != '\n' {
-                        token_value.push(self.current_char);
-                        self.advance();
-                    }
-
-                    token_kind = match heading_id {
-                        1 => token::TokenKind::Heading1,
-                        2 => token::TokenKind::Heading2,
-                        3 => token::TokenKind::Heading3,
-                        4 => token::TokenKind::Heading4,
-                        5 => token::TokenKind::Heading5,
-                        6 => token::TokenKind::Heading6,
-                        _ => token::TokenKind::Paragraph,
-                    };
-                }
-                '`' => {
-                    if !last_paragraph.is_empty() {
-                        res.push(self.create_paragraph(&last_paragraph));
-                        last_paragraph = String::new();
-                    }
-                    if self.peek_equals('`') {
-                        self.advance();
-                        if self.peek_equals('`') {
-                            let mut code_lang = String::new();
-                            self.advance();
-                            self.advance();
-                            while self.current_char != '\n' {
-                                code_lang.push(self.current_char);
-                                self.advance();
-                            }
-
-                            while self.current_char != '`' {
-                                token_value.push(self.current_char);
-                                self.advance();
-                            }
-
-                            token_kind = token::TokenKind::CodeBlock(code_lang);
-                        }
-                    } else {
-                        self.advance();
-                        while self.current_char != '`' {
-                            token_value.push(self.current_char);
-                            self.advance();
-                        }
-                        token_kind = token::TokenKind::CodeInline;
-                        // consume `
-                        self.advance();
-                    }
-                }
-                // TODO: parse blockquotes
-                // '>' => { }
-                '[' => {
-                    if !last_paragraph.is_empty() {
-                        res.push(self.create_paragraph(&last_paragraph));
-                        last_paragraph = String::new();
-                    }
-                    if self.peek_equals(']') {
-                        last_paragraph.push(self.current_char);
-                        self.advance();
-                        continue;
-                    }
-
-                    let mut link_title = String::new();
-                    while self.current_char != ']' && self.current_char != '\n' {
-                        link_title.push(self.current_char);
-                        self.advance();
-                    }
-
-                    if self.current_char == '\n' {
-                        last_paragraph.push_str(&link_title);
-                        continue;
-                    }
-
-                    token_value = link_title;
-                    // TODO:
-                    // 1. check if next char is (
-                    // 2. consume all chars until ) and store them in token::TokenKind::Link
-                }
-                '_' => {
-                    if !last_paragraph.is_empty() {
-                        res.push(self.create_paragraph(&last_paragraph));
-                        last_paragraph = String::new();
-                    }
-                    // consume opening '_'
-                    self.advance();
-                    token_kind = token::TokenKind::Italic;
-                    while self.current_char != '_' {
-                        token_value.push(self.current_char);
-                        self.advance();
-                    }
-                    // consume closing '_'
-                    self.advance();
-                }
-                '-' => {
-                    if self.last_char != '\n' {
-                        last_paragraph.push(self.current_char);
-                        self.advance();
-                        continue;
-                    }
-                    let mut minus_amount = 1;
-                    self.advance();
-
-                    while self.current_char == '-' {
-                        minus_amount += 1;
-                        self.advance();
-                    }
-
-                    match minus_amount {
-                        x if x >= 3 => {
-                            if !last_paragraph.is_empty() {
-                                res.push(self.create_paragraph(&last_paragraph));
-                                last_paragraph = String::new();
-                            }
-                            res.push(self.create_token(token::TokenKind::Ruler, String::new()));
-                            self.advance();
-                            continue;
-                        }
-                        1 => {
-                            self.advance();
-                            match self.current_char {
-                                // match todo list
-                                '[' => {
-                                    if !last_paragraph.is_empty() {
-                                        res.push(self.create_paragraph(&last_paragraph));
-                                        last_paragraph = String::new();
-                                    }
-                                    // skip [
-                                    self.advance();
-                                    // if current char is x, list is done, otherwise not done
-                                    token_kind =
-                                        token::TokenKind::CheckListItem(self.current_char == 'x');
-                                    // advance to ]
-                                    self.advance();
-                                    // advance to line content
-                                    self.advance();
-                                    while self.current_char != '\n' {
-                                        token_value.push(self.current_char);
-                                        self.advance();
-                                    }
-                                    res.push(self.create_token(token_kind, token_value));
-                                    continue;
-                                }
-                                // match unordered list
-                                _ => {
-                                    if !last_paragraph.is_empty() {
-                                        res.push(self.create_paragraph(&last_paragraph));
-                                        last_paragraph = String::new();
-                                    }
-                                    while self.current_char != '\n' {
-                                        token_value.push(self.current_char);
-                                        self.advance();
-                                    }
-                                    res.push(
-                                        self.create_token(token::TokenKind::Listitem, token_value),
-                                    );
-                                    continue;
-                                }
-                            }
-                        }
-                        _ => {
-                            continue;
-                        }
-                    }
-                }
-                '*' => {
-                    if self.peek_equals('*') {
-                        if !last_paragraph.is_empty() {
-                            res.push(self.create_paragraph(&last_paragraph));
-                            last_paragraph = String::new();
-                        }
-                        token_kind = token::TokenKind::Bold;
-                        // consume opening '*'
-                        self.advance();
-
-                        // check for horizontal ruler
-                        if self.peek_equals('*') {
-                            if !last_paragraph.is_empty() {
-                                res.push(self.create_paragraph(&last_paragraph));
-                                last_paragraph = String::new();
-                            }
-                            res.push(self.create_token(token::TokenKind::Ruler, String::new()));
-                            self.advance();
-                            continue;
-                        }
-
-                        // consume second opening '*'
-                        self.advance();
-                        while self.current_char != '*' {
-                            token_value.push(self.current_char);
-                            self.advance();
-                        }
-                        // consume closing '*'
-                        self.advance();
-                    }
-                }
-                _ => {
-                    last_paragraph.push(self.current_char);
-                    self.advance();
-                    continue;
-                }
+                _ => {}
             }
 
-            if token_kind != token::TokenKind::Unknown {
-                res.push(self.create_token(token_kind, token_value))
-            }
             self.advance();
         }
         res
--- src/token.rs
+++ src/token.rs
@@ -1,44 +1,16 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum TokenKind {
-    Unknown,
-    /// <p>paragraph</p> / paragraph
-    Paragraph,
-    /// <h1>heading</h1> / # heading
-    Heading1,
-    /// <h2>heading</h2> / ## heading
-    Heading2,
-    /// <h3>heading</h4> / ### heading
-    Heading3,
-    /// <h4>heading</h4> / #### heading
-    Heading4,
-    /// <h5>heading</h5> / ##### heading
-    Heading5,
-    /// <h6>heading</h6> / ###### heading
-    Heading6,
-    /// <blockquote>quote</blockquote> / > quote
-    Quote,
-    /// <li>list element</li> / - list element
-    Listitem,
-    /// <li>list element</li> / - list element
-    OrderedListitem,
-    /// <li><input type="checkbox" disabled>list element</input></li> / - list element
-    CheckListItem(bool),
-    /// <code>code</code> / `code`
-    CodeInline,
-    /// <pre><code>console.log("test")</code></pre> / ```js\nconsole.log("test")```
-    /// value in codeblock includes the lang of the code block
-    CodeBlock(String),
-    /// <hr> / ---
-    Ruler,
-    /// <b>text</b> / **text**
-    Bold,
-    /// <i>text</i> / _text_
-    Italic,
-    /// <img src="path"/> / ![image](path)
-    Image,
-    /// <a href="path">name</a> / [name](path)
-    /// value in link includes the link to point to
-    Link(String),
+    Hash,
+    Underscore,
+    Star,
+    Newline,
+    Dash,
+    StraightBraceOpen,
+    StraightBraceClose,
+    ParenOpen,
+    ParenClose,
+    BackTick,
+    Text,
 }
 
 #[derive(Debug)]
