e49086dfa4696c1eb8cea6a342277f4fa9916b34
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-04-05 10:44:04 +0200

    test: for all currently supported markdown features
    tests:
        - headings
        - quotes
        - bold / emphasis
        - italics
        - horizontal rulers
        - links
        - images
        - inline code elements

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -4,22 +4,94 @@ import (
 	"testing"
 )
 
-// TODO: add tests for each token:
-//	- HASH
-//	- UNDERSCORE
-//	- STAR
-//	- NEWLINE
-//	- DASH
-//	- STRAIGHTBRACEOPEN
-//	- STRAIGHTBRACECLOSE
-//	- PARENOPEN
-//	- PARENCLOSE
-//	- BACKTICK
-//	- GREATERTHAN
-//	- TEXT
-//	- EMPTYLINE
-
 func BenchmarkReadme(b *testing.B) {
-	s := NewScanner("../test.md")
+	s := NewScanner("../README.md")
 	s.Parse()
+}
+
+func TestHeadings(t *testing.T) {
+	s := NewScanner("./tests/markdown.md")
+	s.Parse()
+	tokens := s.Tokens()
+	expectedTokens := []uint{
+		HASH,
+		TEXT,
+		NEWLINE,
+		HASH,
+		HASH,
+		TEXT,
+		NEWLINE,
+		HASH,
+		HASH,
+		HASH,
+		TEXT,
+		NEWLINE,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		TEXT,
+		NEWLINE,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		TEXT,
+		NEWLINE,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		HASH,
+		TEXT,
+		NEWLINE,
+		GREATERTHAN,
+		TEXT,
+		NEWLINE,
+		STAR,
+		STAR,
+		TEXT,
+		STAR,
+		STAR,
+		TEXT,
+		UNDERSCORE,
+		TEXT,
+		UNDERSCORE,
+		NEWLINE,
+		DASH,
+		DASH,
+		DASH,
+		NEWLINE,
+		STRAIGHTBRACEOPEN,
+		TEXT,
+		STRAIGHTBRACECLOSE,
+		PARENOPEN,
+		TEXT,
+		PARENCLOSE,
+		NEWLINE,
+		BANG,
+		STRAIGHTBRACEOPEN,
+		TEXT,
+		STRAIGHTBRACECLOSE,
+		PARENOPEN,
+		TEXT,
+		PARENCLOSE,
+		NEWLINE,
+		BACKTICK,
+		TEXT,
+		BACKTICK,
+		NEWLINE,
+	}
+	if len(tokens) != len(expectedTokens) {
+		t.Errorf("expected %d tokens, got: %d", len(expectedTokens), len(tokens))
+	}
+	for i, token := range tokens {
+		if expectedTokens[i] != token.Kind {
+			t.Errorf("expected %d [%s], got %d [%s] for token %d",
+				expectedTokens[i], TOKEN_LOOKUP_MAP[expectedTokens[i]], token.Kind, TOKEN_LOOKUP_MAP[token.Kind], i,
+			)
+		}
+	}
 }
---
+++ scanner/tests/markdown.md
@@ -0,0 +1,22 @@
+# heading 1
+
+## heading 2
+
+### heading 3
+
+#### heading 4
+
+##### heading 5
+
+###### heading 6
+
+> this is a quote
+
+**emphasis** _italic_
+
+---
+
+[xnacly](https://xnacly.me/)
+![xnacly](https://avatars.githubusercontent.com/u/47723417?v=4)
+
+`inline code`
