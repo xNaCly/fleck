cb970804407e8107bd78f3baf26ddb1489591760
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-02 14:42:34 +0200

    feat(generator): add katex injection script
    Currently fleck accepts valid katex latex in between $$ and  between $$$$, the former has displaymath disabled, the second has it enabled

--- generator/generator.go
+++ generator/generator.go
@@ -267,6 +267,10 @@ func WriteTemplate(fileName string, result []parser.Tag, toc string) {
 		writer.WriteString(`<link href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism.min.css" rel="stylesheet">`)
 	}
 
+	if cli.ARGUMENTS.GetFlag("math") {
+		writer.WriteString(`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css" integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI" crossorigin="anonymous"> <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.js" integrity="sha384-G0zcxDFp5LWZtDuRMnBkk3EphCK1lhEf4UEyEM693ka574TZGwo4IWwS6QLzM/2t" crossorigin="anonymous"></script> <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous" onload="renderMathInElement(document.body, {delimiters: [{left: '$$', right: '$$', display: true}, {left:'$', right:'$', display: false}]});"></script>`)
+	}
+
 	writer.WriteString("</head><body><div class='main'>")
 
 	if len(toc) != 0 {
